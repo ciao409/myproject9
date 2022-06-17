83a734ec19f3c1c78013825cef37091cf4369969
ciao409 <yuciao276@gmail.com>
2022-06-17 15:11:38 +0800

    student with kotlin

---
+++ src/main/java/com/joy/score/student.kt
@@ -0,0 +1,18 @@
+package com.joy.score
+
+class student(var name: String,var english:Int,var math:Int) {
+    init {
+ //       println("Testing")
+    }
+    fun print(){
+        println("$name\t$english \t$math\t${getAverage()}")
+    }
+
+    fun getAverage() =(english+math)/2
+
+}
+
+fun main(){
+        val stu =student("Jack",49,64)
+        stu.print()
+}
